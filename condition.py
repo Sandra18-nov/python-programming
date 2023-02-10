707e987604e8cbda64754f2e6f130cd5c0b12492
sandra18-nov <sandramugucia@gmail.com>
2023-02-10 10:54:54 +0300

    python lists

--- condition.py
+++ condition.py
@@ -2,7 +2,7 @@ num=eval(input("enter a number: "))
 if num<0:
     print("wrong input")
 else:
-    if num<50:
+    if num>50:
         x =num/2
         print(x) 
     else:
---
+++ lists.py
@@ -0,0 +1,51 @@
+# #my first python list
+# numbers =[2,3,4,56,56]
+# for i in range(len(numbers)):
+#     print(numbers[i])
+
+#     #LIST METHODS
+#     #index
+#     (numbers.index(56))
+
+    
+#     # Append = adds an element to the end of the list
+# for i in range(len(numbers)):
+#     print(numbers[i])
+
+#     numbers.append(36)
+
+#     #after appending
+# for i in range(len(numbers)):
+#     print(numbers[i])
+
+#     # count =returns the number or times an element occurs in a list
+# print(numbers.count(56))
+
+# #pop =removes an element at a given index and returns that element
+# print(numbers.pop(4))
+
+# #INSERT=inserts an element x at index y 
+# numbers.insert(4,78)
+
+# for i in range(len(numbers)):
+#  print(numbers[i])
+
+#  #SORT=arranges the list in ascending order
+# for i in range(len(numbers)):
+#     print(numbers[i])
+
+# #sort in the list 
+# numbers.sort()
+
+# #creating lists
+# nums=[]#empty list 
+
+# for i in range(10):
+# #insert elements into the list 
+#   nums.append(i)
+
+#   for i in range(len(nums)):
+#     print(nums[i])
+/ print(nums[i])
+
+
