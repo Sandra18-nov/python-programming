03b1fece811ac35b6f82dc7b01532c754c091360
sandra18-nov <sandramugucia@gmail.com>
2023-02-14 11:01:05 +0300

    tuesdays

---
+++ arrays.py
@@ -0,0 +1,40 @@
+numbers = [3,5,6,7,8]
+x = numbers [1]
+print(x)
+
+numbers=[12,45,67,89]
+numbers[3]=9
+print(numbers)
+
+cars=["jeep","ford","chryseller"]
+cars.append("toyota")
+print(cars)
+
+numbers=[3,4,55,6]
+numbers.pop(3)
+print(numbers)
+
+numbers=[4,5,6,7,8,9]
+length=len(numbers)
+print(length)
+
+print(len("potato"))
+
+numbers=[4,5,6,7,8,9,7]
+length=numbers.count(7)
+print(length)
+
+numbers=[4,5,6,7,8,9,7]
+numbers.reverse()
+print(numbers)
+
+numbers=[4,5,6,7,8,9,7]
+for i in numbers:
+    print(i)
+
+numbers=[4,5,6,7,8,9,7]
+for i in range(3):
+    print(numbers[i])
+
+
+
---
+++ strings.py
@@ -0,0 +1,65 @@
+# name="climate"
+# print(name)
+
+# #index
+# name[1]
+# print(name[1])
+
+# name="climate"
+# print(len(name))
+
+# name="climate"
+# name=[-1]
+# print(name[-1])
+
+# name="climate"
+# for letter in name:
+#     print(letter)
+    
+
+# print("_"*5)
+
+# name1="peter"
+# name2="joy"
+# print(name1+name2)
+
+# name=input("enter name: ")
+# print("hello "+ name)
+
+# name="hellen"
+# name1="david"
+# print(name)
+# print(name1)
+
+# name="hellen"
+# name.upper()
+# print(name.upper())
+
+# name="FORD"
+# name.lower()
+# print(name.lower())
+
+# #checking for alphanumeric
+# x="ford"
+# print(x.isalpha())
+
+# password="string04"
+# if password.isalpha():
+#     print("invalid password")
+# else:
+#     print("valid password")
+
+
+#ask for username
+#print "hello username"
+#use a function 
+#hello EDWIN
+
+# name=input("enter your name: ")
+# print("hello " +name)
+
+# name="x123edwin5776"
+# print(name[4:9])
+
+# email="bob123@gmail.com"
+# print(email[-1:-2])
